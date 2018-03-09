7505752b55c4cf96b22310d49f504d68f5780b2b
livhow <livyb.how@gmail.com>
2018-03-09 10:24:36 -0800

    Unit10

---
+++ LabAssessment/src/LabAssessment.java
@@ -0,0 +1,19 @@
+
+public class LabAssessment
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hi");
+		System.out.println(Method1());
+		System.out.println(Method2());
+	}
+	
+	public static String Method1()
+	{
+		return "Hello";
+	}
+	public static int Method2()
+	{
+		return 4;
+	}
+}
