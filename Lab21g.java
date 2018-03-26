f3b256f4b978b9caf39aaa5716404598d4f963da
livhow <livyb.how@gmail.com>
2018-03-26 09:50:31 -0700

    Unit14Upload

---
+++ Lab21g.java
@@ -0,0 +1,23 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Lab21g
+{
+	public static void main(String args[])
+	{
+		//instantiate an AtCounter
+
+		//test the code
+		AtCounter test = new AtCounter();
+		test.countAts(0,0);
+		out.println(test);
+	}
+}
